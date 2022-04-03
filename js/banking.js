3db80afde40ec2e5613d0bd71a45fe226a3443b6
khaled39ab <khaled39ab@gmail.com>
2022-04-03 17:35:30 +0600

    withdraw Amount working

--- js/banking.js
+++ js/banking.js
@@ -27,3 +27,31 @@ document.getElementById ("deposit-button").addEventListener("click", function(){
     document.getElementById ("deposit-input").value = "";
 });
 
+// withdraw button event handler
+document.getElementById ("withdraw-button").addEventListener("click", function(){
+    const withdrawInput = document.getElementById("withdraw-input");
+    const withdrawAmount = withdrawInput.value;
+    const previousWithdrawAmount = parseFloat(withdrawAmount);
+    
+    // get withdraw amount
+    if (typeof withdrawAmount == 'number' || withdrawAmount > 0){
+        const withdrawTotal = document.getElementById("withdraw-total");
+        const withdrawTotalText = withdrawTotal.innerText;
+        const previousWithdrawTotal = parseFloat(withdrawTotalText);
+        
+        withdrawTotal.innerText = previousWithdrawAmount + previousWithdrawTotal;
+
+        //handle total balance
+        const balanceTotal = document.getElementById("balance-total");
+        const balanceTotalText = balanceTotal.innerText;
+        const previousBalanceTotal = parseFloat(balanceTotalText);
+
+        balanceTotal.innerText = previousBalanceTotal - previousWithdrawAmount;
+    }
+    else{
+        alert("Wrong Input")
+    }
+    
+    // clear withdraw input 
+    document.getElementById("withdraw-input").value = '';
+})
